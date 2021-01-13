60c103632f202cd0e08d6bb2e8687268228badad
ssue0625 <ssue0625@gmail.com>
2021-01-13 17:26:35 +0900

    spring boot 프로젝트를 war로 배포 하기 위해 수정

--- src/main/java/com/example/web/BoardBootApplication.java
+++ src/main/java/com/example/web/BoardBootApplication.java
@@ -2,9 +2,16 @@ package com.example.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BoardBootApplication {
+public class BoardBootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BoardBootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardBootApplication.class, args);
